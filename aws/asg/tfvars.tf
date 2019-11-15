36cb24278f2acf2defea259325695ace91f2ac89
Luke Hertert <luke.hertert@gmail.com>
2019-11-15 12:18:43 -0500

    Create tfvars.tf

---
+++ aws/asg/tfvars.tf
@@ -0,0 +1,62 @@
+variable "name" {
+  description = "The name of this asg"
+  default     = "harness"
+}
+
+variable "vpc_id" {
+  description = ""
+  default     = ""
+}
+
+variable "max_size" {
+  default = 1
+}
+
+variable "min_size" {
+  default = 1
+}
+
+variable "desired_capacity" {
+  default = 1
+}
+
+variable "instance_security_groups" {
+  default = []
+}
+
+variable "subnet_ids" {
+  default = []
+}
+
+variable "key_name" {
+  default = ""
+}
+
+variable "image_id" {
+  default = ""
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "lb_security_groups" {
+  default = []
+}
+
+variable "lb_listen_port" {
+  default = 80
+}
+
+variable "lb_listen_protocol" {
+  default = "HTTP"
+}
+
+variable "instance_port" {
+  default = "80"
+}
+
+
+
+# Other variables
+# tags
