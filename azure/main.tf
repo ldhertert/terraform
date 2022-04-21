49f84612020d6dbf9864f94190ba6f43026ade81
Luke Hertert <luke.hertert@gmail.com>
2022-04-21 12:17:43 -0400

    Add files via upload

---
+++ azure/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "cluster" {
+  source = "./modules/aks"
+
+  name           = "azuretf"
+  resource_group = "harness-automation"
+  tags = {
+    env        = "dev"
+    created_by = "luke"
+  }
+}
+
+
+module "registry" {
+    source  = "./modules/acr"
+
+    name = "azuretf"
+    resource_group = "harness-automation"
+    tags = {
+        env = "dev"
+        created_by = "luke"
+    }
+}
+
+resource "azurerm_role_assignment" "aks_acr_role" {
+  principal_id                     = module.cluster.cluster_identity
+  role_definition_name             = "AcrPull"
+  scope                            = module.registry.id
+  skip_service_principal_aad_check = true
+}
+
+# module "webapp" {
+#   source = "./modules/app_service"
+
+#   name           = "azuretf"
+#   resource_group = "harness-automation"
+#   tags = {
+#     env        = "dev"
+#     created_by = "luke"
+#   }
+# }
+
+# module "vault" {
+#   source = "./modules/azure_key_vault"
+
+#   name           = "azuretf"
+#   resource_group = "harness-automation"
+#   tags = {
+#     env        = "dev"
+#     created_by = "luke"
+#   }
+# }
+
