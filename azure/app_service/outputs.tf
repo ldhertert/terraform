5325360b6722c238df323ee33fa08df37ee9b0de
Luke Hertert <luke.hertert@gmail.com>
2022-09-29 15:03:57 -0400

    Add more outputs to app service module

--- azure/app_service/outputs.tf
+++ azure/app_service/outputs.tf
@@ -5,3 +5,19 @@ output "primary_host" {
 output "staging_host" {
   value = azurerm_linux_web_app.app.default_hostname
 }
+
+output "app_id" {
+  value = azurerm_linux_web_app.app.id
+}
+
+output "app_name" {
+  value = azurerm_linux_web_app.app.name
+}
+
+output "primary_slot_name" {
+  value = azurerm_linux_web_app.app.name
+}
+
+output "staging_slot_name" {
+  value = azurerm_linux_web_app_slot.slot.name
+}
